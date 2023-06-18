8e1f4feb286ad2e7bbaa4293f40f60c1a1b35a67
Mayank <myank07official@gmail.com>
2023-06-18 15:18:30 +0530

    updated index.js

--- portfolio/src/Components/About_Card.jsx
+++ portfolio/src/Components/About_Card.jsx
@@ -4,7 +4,7 @@ function  AboutCard(){
     return (
         <div>
             <div  className='about_card'>
-                <div data-aos='fade-right' class="abt_card" style={{boxShadow: "-11px 12px 26px -19px black, 14px -11px 19px -19px black",height:"263px",marginTop:"80px"}}>
+                <div data-aos='fade-right' class="abt_card" style={{boxShadow: "-11px 12px 26px -19px black, 14px -11px 19px -19px black",height:"350px",marginTop:"80px"}}>
                 {/* <img src="./images/drum.gif" class="card-img-top" alt="..."/> */}
                 <div class="card-body_abt">
                  <h5 class="card-title">Mayank Uppal</h5>
