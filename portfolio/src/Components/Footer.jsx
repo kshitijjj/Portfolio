6eb5c9808074accc8687e7abd6bb76f458f994f9
Mayank <myank07official@gmail.com>
2023-06-18 15:20:39 +0530

    updated index.js

--- portfolio/src/Components/Footer.jsx
+++ portfolio/src/Components/Footer.jsx
@@ -11,10 +11,10 @@ function Footer(){
                 copyright2023
             </div>
             <div className="socialss">
-                <a href="https://www.instagram.com/__.mayank.___07/"><img id='I1' src="./images/f4.svg" alt="" width={"40px"} height={"40px"} /></a>
+                {/* <a href="https://www.instagram.com/__.mayank.___07/"><img id='I1' src="./images/f4.svg" alt="" width={"40px"} height={"40px"} /></a>
                 <a href="https://www.linkedin.com/in/mayank-uppal-3591a5229"><img id='I2' src="./images/f1.svg" alt=""  width={"40px"} height={"40px"}/></a>
                 <a href="https://twitter.com/Uppal__MaYank?t=C4C3TRkRVvdiPkbm6N0uKA&s=09"><img id='I3' src="./images/f3.svg" alt=""  width={"40px"} height={"40px"}/></a>
-                <a href="https://github.com/kshitijjj"><img id='I4' src="./images/f2.svg" alt=""  width={"40px"} height={"40px"} /></a>
+                <a href="https://github.com/kshitijjj"><img id='I4' src="./images/f2.svg" alt=""  width={"40px"} height={"40px"} /></a> */}
 
             </div>
         </div>
