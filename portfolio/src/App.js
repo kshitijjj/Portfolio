5e03d5a3fb45e8945e75dd71e7c7c9333d5a287a
Mayank <myank07official@gmail.com>
2023-06-19 18:04:06 +0530

    update app.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -8,23 +8,9 @@ import About from "./Components/About";
 import Footer from "./Components/Footer";
 
 
-/* function useWindowSize(){
-  const[size,setSize]=useState([window.innerHeight,window.innerWidth]);
-  useEffect(()=>{
-    const handleResize=()=>{
-      setSize([window.innerHeight,window.innerWidth]);
-    };
-    window.addEventListener("resize",handleResize);
-    return()=>{
-      window.removeEventListener("resize",handleResize);
-    }
-  },[])
-  return size;
-
-} */
 
 function App() {
- /*  const[height,width]=useWindowSize(); */
+ 
   return (
     <> 
     <Navbar sticky='top'/>
