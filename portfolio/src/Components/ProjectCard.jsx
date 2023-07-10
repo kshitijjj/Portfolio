74a6fb1542724c042d20e157976d0d3a73f55325
Mayank <myank07official@gmail.com>
2023-07-10 17:25:13 +0530

    update project component

--- portfolio/src/Components/ProjectCard.jsx
+++ portfolio/src/Components/ProjectCard.jsx
@@ -5,7 +5,7 @@ function ProjectCard(props){
       <>
 
 <div class="card" style={{width: "18rem"}}>
-  <img class="card-img-top" src={props.src} alt="Card image cap"/>
+  <img class={props.className} src={props.src} alt="Card image cap"/>
   <div class="card-body">
     <h5 class="card-title">{props.title}</h5>
     <p class="card-text">{props.desc}</p>
