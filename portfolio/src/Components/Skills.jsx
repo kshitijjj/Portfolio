bf6c54bd9f58c6ba2b4dba1d2af800f313823dfe
Mayank <myank07official@gmail.com>
2023-07-10 18:21:46 +0530

    update Skills Component

--- portfolio/src/Components/Skills.jsx
+++ portfolio/src/Components/Skills.jsx
@@ -3,9 +3,12 @@ import Images from './Images';
 import AOS from "aos";
 import "aos/dist/aos.css";
 import SkillsCard from './Skills_Card';
+import { useState } from 'react';
+
 
 
 function Skills(props) {
+
         useEffect(()=>{
             AOS.init({duration:1500});
         },[]);
@@ -18,16 +21,16 @@ function Skills(props) {
             <div className="skills_main">
 
             <div className="skills_progress">
-            <SkillsCard skills_text="85%"/>
-            <SkillsCard skills_text="85%"/>
-            <SkillsCard skills_text="85%"/>
-            <SkillsCard skills_text="85%"/>
+            <SkillsCard skills_card_text="Html" skills_text="85%"/>
+            <SkillsCard skills_card_text="Css"  skills_text="85%"/>
+            <SkillsCard skills_card_text="Javascript"  skills_text="85%"/>
+            <SkillsCard skills_card_text="ReactJs"  skills_text="85%"/>
             </div>
 
             <div className="skills_progress_1">
-            <SkillsCard skills_text="85%"/>
-            <SkillsCard skills_text="85%"/>
-            <SkillsCard/>
+            <SkillsCard skills_card_text="NodeJs" skills_text="85%"/>
+            <SkillsCard skills_card_text="ExpressJs" skills_text="85%"/>
+            <SkillsCard skills_card_text="MongoDB" skills_text="85%"/>
             </div>
             
             </div>
--- portfolio/src/Components/Skills_Card.jsx
+++ portfolio/src/Components/Skills_Card.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 
 function SkillsCard(props){
     return (
+        <>
+        <div className="skills_card_main">
+        <p id="skills_card_text">{props.skills_card_text}</p>
         <div class="progress" style={{height:"3.5vh",width:"40vw"}} >
-  <div class="progress-bar" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style={{fontfamily:'Belanosima,sans-serif'}}>{props.skills_text} </div>
+  <div class="progress-bar" role="progressbar" aria-valuenow="25"  aria-valuemin="0" aria-valuemax="100">{props.skills_text} </div>
 </div>
+</div>
+</>
     );
 }
 
