739034cd7250bd6acac10d92f6cf730ba6f7c24b
Mayank <myank07official@gmail.com>
2023-07-11 14:41:22 +0530

    update index.css

--- portfolio/src/Components/About_Card.jsx
+++ portfolio/src/Components/About_Card.jsx
@@ -2,7 +2,25 @@ import React from 'react';
 
 function  AboutCard(props){
     return (
-        <div id="about" className='about_title'>
+        <>
+        <div className="about_heading1">
+            ABOUT
+        </div>
+        <div className="about_main">
+
+            {/* <div className="about_image">
+                <img id='about_img1' src="./images/pic.jpeg" alt="" srcset="" />
+            </div> */}
+
+            <div className="about_main_2">
+                <div className="about_heading">
+                    <p id="about_text1">MAYANK UPPAL</p>
+                    <p id="about_text_2">Currently, I am a third year undergraduate student at Punjab Engineering College, Chandigarh</p>
+                </div>
+            </div>
+        </div>
+        </>
+        /* <div id="about" className='about_title'>
             <div  className='about_card'>
                 <div data-aos='fade-right' class="abt_card">
                 <div class="card-body_abt">
@@ -15,7 +33,8 @@ function  AboutCard(props){
              </div>
             </div>
         </div>
-    );
+    ); */
+    )
 }
 
 export default AboutCard;
