2af626830440641deb32d6438ad3bc4449030e43
Mayank <myank07official@gmail.com>
2023-07-11 15:13:54 +0530

    update index.css

--- portfolio/src/Components/About_Card.jsx
+++ portfolio/src/Components/About_Card.jsx
@@ -3,37 +3,20 @@ import React from 'react';
 function  AboutCard(props){
     return (
         <>
-        <div className="about_heading1">
-            ABOUT
-        </div>
         <div className="about_main">
 
-            {/* <div className="about_image">
-                <img id='about_img1' src="./images/pic.jpeg" alt="" srcset="" />
-            </div> */}
+            <p id="about_heading">ABOUT</p>
 
-            <div className="about_main_2">
-                <div className="about_heading">
+            <div className="about_main1">
+                <img src="./images/pic.jpeg" alt="" id="about_img" />
+                <div className="about_text">
                     <p id="about_text1">MAYANK UPPAL</p>
-                    <p id="about_text_2">Currently, I am a third year undergraduate student at Punjab Engineering College, Chandigarh</p>
+                    <p id="about_text2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis odit recusandae, ad vitae possimus rerum cupiditate qui quisquam rem, itaque nulla beatae vero error ratione?</p>
                 </div>
             </div>
+
         </div>
         </>
-        /* <div id="about" className='about_title'>
-            <div  className='about_card'>
-                <div data-aos='fade-right' class="abt_card">
-                <div class="card-body_abt">
-                <div className="about_card_text">
-                 <p id="abt_txt" class="card-text">{props.desc}</p>
-                 <div className='link'>
-                </div>
-                </div>
-                </div>
-             </div>
-            </div>
-        </div>
-    ); */
     )
 }
 
